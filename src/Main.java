79c8e886b4ac5aa34aab0e8265dbed9b3bfd1ed4
SvetlanaDD <asweet.lana@gmail.com>
2022-09-14 15:31:03 +0300

    Add Predicate

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        MyComparator comparator = new MyComparator(3);
 
-        List<Person> persons = Arrays.asList(
-                new Person("Богдан", "Фон Кроннинг Белл", 82),
-                new Person("Николай", "Римский-Корсаков", 50),
-                new Person("Адель", "Принц Аль-Рейми-Шувейх", 18),
-                new Person("Иван", "Иванов", 48));
+        ArrayList<Person> persons = new ArrayList<Person>();
 
-        Collections.sort(persons, comparator);
+        persons.add(new Person("Богдан", "Фон Кроннинг Белл", 82));
+        persons.add(new Person("Николай", "Римский-Корсаков", 50));
+        persons.add(new Person("Петр", "Краснов", 12));
+        persons.add(new Person("Адель", "Принц Аль-Рейми-Шувейх", 18));
+        persons.add(new Person("Алекс", "Штерн", 14));
+        persons.add(new Person("Иван", "Иванов", 48));
+
+        Predicate<Person> isAdult = person -> person.getAge() < 18;
+        persons.removeIf(isAdult);
 
         for (Person s : persons) {
             System.out.println(s);
